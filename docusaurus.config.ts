1c1d60fade7f2db72e5ddba6c439d779d6bbb4c4
Trần Trung Hiếu <trantrunghieu2393@gmail.com>
2026-06-03 11:52:04 +0700

    Add trailingSlash configuration to Docusaurus config for improved URL handling

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -14,6 +14,8 @@ const config: Config = {
   tagline: "Dinosaurs are cool",
   favicon: "img/favicon.ico",
 
+  trailingSlash: false,
+
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
     v4: true, // Improve compatibility with the upcoming Docusaurus v4
